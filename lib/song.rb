f85ceeff64d3bce38ac0a854d8a4ff05662e2de1
Ryan Manchester <ryanm17@gmail.com>
2018-04-27 01:03:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,6 +17,7 @@ class Song
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
+      binding.pry
       column_names << row["name"]
     end
     column_names.compact
